8ee9672d4b7b2428069c64447e8fd9962306fb8f
Arka Roy <arka243roy@googlemail.com>
2019-05-03 13:49:35 +0100

    Updated constructors example file

--- concepts/Exercises/constructors_example.py
+++ concepts/Exercises/constructors_example.py
@@ -23,4 +23,7 @@ print((c2.real, c2.imag, c2.attr))
 
 # but c1 object doesn't have attribute 'attr'
 # AttributeError: 'ComplexNumber' object has no attribute 'attr'
-c1.attr
+c1 = ComplexNumber(2,3)
+del c1.imag
+del ComplexNumber.getData
+c1.getData()
